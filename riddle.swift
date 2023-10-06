3eca9383a731d4c8edbec8fffae0bbaaca5983e8
AlexStich <alex@rucode.org>
2023-10-06 17:48:35 +0400

    Add Riddle

---
+++ riddle.swift
@@ -0,0 +1,15 @@
+class A {
+    func execute(param: Int = 0) {
+        print("A: \(param)")
+    }
+
+}
+
+class B: A {
+    override func execute(param: Int = 1) {
+        print("B: \(param)")
+    }
+}
+
+let instance: A = B()
+instance.execute()
